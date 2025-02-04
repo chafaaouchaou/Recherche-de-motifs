b8c0f4adf534dd2a74493d98c545a57ca05a22ac
chafaa <chafaaouchaou@gmail.com>
2025-02-04 13:47:28 +0100

    updating the file package

--- Backend/src/main/java/com/projetarchi/recherchemotifs/Produit/Controlleur.java
+++ Backend/src/main/java/com/projetarchi/recherchemotifs/Produit/Controlleur.java
@@ -1,4 +1,4 @@
-package com.projetarchi.exo1.Produit;
+package com.projetarchi.recherchemotifs.Produit;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
