d3a55ef6ad269933957ea0057bc19f357c5df957
chafaa <chafaaouchaou@gmail.com>
2025-02-17 19:01:38 +0100

    fixing front build issues

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { BarChart, XAxis, YAxis, Bar, Tooltip, Legend } from 'recharts';
 import { Check, Loader2, Download } from 'lucide-react';
 import ReactMarkdown from 'react-markdown';
