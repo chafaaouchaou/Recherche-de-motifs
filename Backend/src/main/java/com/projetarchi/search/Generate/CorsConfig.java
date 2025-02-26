6142db6c298e2519649b90fd2cbd74f593a63fab
chafaa <chafaaouchaou@gmail.com>
2025-02-26 22:36:55 +0100

    updating cors

--- Backend/src/main/java/com/projetarchi/search/Generate/CorsConfig.java
+++ Backend/src/main/java/com/projetarchi/search/Generate/CorsConfig.java
@@ -9,28 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") 
-                .allowedMethods("*") 
-                .allowedHeaders("*");
+                .allowedOrigins("https://pattern-matching.chafaaouchaou.online") // Autoriser uniquement cette origine
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Définir les méthodes autorisées
+                .allowedHeaders("*")
+                .allowCredentials(true); // Permettre l'envoi des cookies si besoin
     }
 }
-
-
-
-// package com.projetarchi.search.Generate;
-
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// @Configuration
-// public class CorsConfig implements WebMvcConfigurer {
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/**")
-//                 .allowedOrigins("https://pattern-matching.chafaaouchaou.online") // Autoriser uniquement cette origine
-//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Définir les méthodes autorisées
-//                 .allowedHeaders("*")
-//                 .allowCredentials(true); // Permettre l'envoi des cookies si besoin
-//     }
-// }
